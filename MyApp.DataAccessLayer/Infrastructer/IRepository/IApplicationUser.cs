94d4cf2be110c5402fa6c2775d06244cecd65556
Twnkle03 <126772421+Twnkle03@users.noreply.github.com>
2023-05-25 11:25:05 +0530

    Add files via upload

---
+++ MyApp.DataAccessLayer/Infrastructer/IRepository/IApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using MyApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccessLayer.Infrastructer.IRepository
+{
+    public interface IApplicationUser:IRepository<ApplicationUser>
+    {
+        
+    }
+}
---
+++ MyApp.DataAccessLayer/Infrastructer/IRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.DataAccessLayer.Infrastructer.IRepository
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
+        ICartRepository Cart { get; }
+        IApplicationUser ApplicationUser { get; }
+        IOrderDetailRepository OrderDetail { get; }
+        IOrderHeaderRepository OrderHeader { get; }
+        void Save();
+    }
+}

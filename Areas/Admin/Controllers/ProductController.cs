45b34cc0c5eaa499057ee5c06e80b167db105126
Twnkle03 <126772421+Twnkle03@users.noreply.github.com>
2023-05-25 12:13:03 +0530

    Add files via upload

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MyApp.CommonHelper;
+using MyApp.DataAccessLayer;
+using MyApp.DataAccessLayer.Infrastructer.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModels;
+
+namespace MyWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = WebSiteRole.Role_Admin)]
+    public class ProductController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        private IWebHostEnvironment _hostingEnviroment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostingEnviroment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostingEnviroment = hostingEnviroment;
+        }
+
+        #region APICALL
+        public IActionResult AllProducts() 
+        {
+            var products = _unitOfWork.Product.GetAll(includeProperties:"Category");
+            return Json(new { data = products });
+        }
+        #endregion
+        public IActionResult Index()
+        {
+            //ProductVM productVM = new ProductVM();
+            //productVM.Products = _unitOfWork.Product.GetAll();
+            return View();
+        }
+
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+
+        //    return View();
+        //}
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Category.Add(category);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Category Created Done!";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(category);
+        //}
+
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            ProductVM vm = new ProductVM()
+            {
+                    Product = new(),
+                    Categories = _unitOfWork.Category.GetAll().Select(x=>
+                    new SelectListItem()
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    })
+            };
+            if (id == null || id == 0)
+            {
+                return View(vm);
+            }
+            else
+            {
+                vm.Product = _unitOfWork.Product.GetT(x => x.Id == id);
+                if (vm.Product == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
+               
+            }
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(ProductVM vm,IFormFile? file)
+        {   
+            if (ModelState.IsValid)
+            {
+                string fileName = String.Empty;
+                if (file!=null)
+                {
+                    string uploadDir = Path.Combine(_hostingEnviroment.WebRootPath, "ProductImage");
+                    fileName = Guid.NewGuid().ToString()+"-"+file.FileName;
+                    string filepath = Path.Combine(uploadDir, fileName);
+
+                    if (vm.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(_hostingEnviroment.WebRootPath, vm.Product.ImageUrl.TrimStart('\\'));
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(filepath, FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    vm.Product.ImageUrl = @"\ProductImage\" + fileName;
+                }
+                if (vm.Product.Id==0) 
+                {
+                    _unitOfWork.Product.Add(vm.Product);
+                    TempData["success"] = "Product Created Done!";
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(vm.Product);
+                    TempData["success"] = "Product Updated Done!";
+                }
+              
+                _unitOfWork.Save();
+               
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        //[HttpGet]
+        //public IActionResult Delete(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var category = _unitOfWork.Category.GetT(x => x.Id == id);
+        //    if (category == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(category);
+        //}
+
+        #region DeleteAPICALL
+        [HttpDelete]
+       
+        public IActionResult Delete(int? id)
+        {
+            var product = _unitOfWork.Product.GetT(x => x.Id == id);
+            if (product == null)
+            {
+                return Json(new { success = false, message = "Error in Fetching Data" });
+            }
+            else
+            {
+                var oldImagePath = Path.Combine(_hostingEnviroment.WebRootPath, product.ImageUrl.TrimStart('\\'));
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+                _unitOfWork.Product.Delete(product);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Product Deleted" });
+            }
+            
+          
+            //TempData["success"] = "Category Deleted Done!";
+            //return RedirectToAction("Index");
+        }
+        #endregion
+    }
+}

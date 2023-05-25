00c2fc301f9894f139d49ed0a961ed4ee3194e98
Twnkle03 <126772421+Twnkle03@users.noreply.github.com>
2023-05-25 12:13:49 +0530

    Add files via upload

---
+++ ViewComponents/CartViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer.Infrastructer.IRepository;
+using System.Security.Claims;
+
+namespace MyWebApp.ViewComponents
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claims != null)
+            {
+                if(HttpContext.Session.GetInt32("SessionCart")!=null) 
+                {
+                    return View(HttpContext.Session.GetInt32("SessionCart"));
+                }
+                else
+                {
+                    HttpContext.Session.SetInt32("SessionCart", _unitOfWork
+                        .Cart.GetAll(x => x.ApplicationUserId == claims.Value).ToList().Count);
+                    return View(HttpContext.Session.GetInt32("Session32"));
+                }
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+           
+        }
+    }
+}
---
+++ ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer.Infrastructer.IRepository;
+
+namespace MyWebApp.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoryViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var categories = _unitOfWork.Category.GetAll().OrderBy(c => c.Name).ToList();
+            return View(categories);
+        }
+    }
+}

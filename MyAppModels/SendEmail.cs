bf0d10df4456dda21f9d0b6fc3acdceef2f3ec1b
Twnkle03 <126772421+Twnkle03@users.noreply.github.com>
2023-05-25 11:27:14 +0530

    Add files via upload

---
+++ MyAppModels/SendEmail.cs
@@ -0,0 +1,45 @@
+﻿//using MailKit.Net.Smtp;
+//using Microsoft.AspNetCore.Identity.UI.Services;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net.Mail;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace MyApp.Models
+//{
+//    public class SendEmail : IEmailSender
+//    {
+//        //public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+//        //{
+//        //    using (MailMessage mailMessage = new MailMessage())
+
+//        //        //MailMessage message = new MailMessage();
+//        //        //SmtpClient smtpClient = new SmtpClient();
+//        //    mailMessage.From = new MailAddress("twinklechaudhari03@gmail.com");
+
+//        //    mailMessage.To.Add(new MailAddress(email));
+//        //    mailMessage.Subject = subject;
+//        //    mailMessage.IsBodyHtml = true;
+//        //    mailMessage.Body = email + htmlMessage;
+
+//        //    SmtpClient smtp = new SmtpClient();
+//        //    smtp.Host = "smtp.gmail.com";
+//        //    smtp.EnableSsl = true;
+//        //    System.Net.NetworkCredential NetworkCred = new System.Net.NetworkCredential();
+//        //    NetworkCred.UserName = "twinklechaudhari03@gmail.com";
+//        //    NetworkCred.Password = "";
+//        //    smtp.UseDefaultCredentials = true;
+//        //    smtp.Credentials = NetworkCred;
+//        //    smtp.Port = 587;
+//        //    await smtp.SendMailAsync(mailMessage);
+
+
+//        //}
+//        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+//        {
+         
+//        }
+//    }
+//}
